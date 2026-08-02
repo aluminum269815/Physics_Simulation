8a14f1890a70c5e3d43ad1db42061120518ef4b8
Jack Jia <jiajack20081113@gmail.com>
2026-08-02 23:24:35 +1200

    added collapsable settings panel with basic parameters coded but without air resistance.

---
+++ basicparameters.py
@@ -0,0 +1,88 @@
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
+class BasicParameters(QWidget):
+
+    def __init__(self, main_window):
+        super().__init__()
+        self.main_window = main_window
+        self.setWindowTitle("Basic Parameters")
+        self.setFixedSize(350, 250)
+
+        air_density_label= QLabel("Air Density")
+
+        self.air_density_slider = QSlider(Qt.Horizontal)
+        self.air_density_slider.setMinimum(0)
+        self.air_density_slider.setMaximum(60000)
+        self.air_density_slider.setValue(1225)
+
+        self.air_density_input = QLineEdit()
+        self.air_density_input.setText("1.225")
+        self.air_density_input.setFixedWidth(60)
+
+        self.air_density_input.editingFinished.connect(self.change_air_density)
+        self.air_density_slider.valueChanged.connect(self.update_air_density)
+
+        gravity_label = QLabel("Gravity(Downward acceleration)")
+
+        self.gravity_slider = QSlider(Qt.Horizontal)
+        self.gravity_slider.setMinimum(0)
+        self.gravity_slider.setMaximum(60000)
+        self.gravity_slider.setValue(9810)
+
+        self.gravity_input = QLineEdit()
+        self.gravity_input.setText("9.81")
+        self.gravity_input.setFixedWidth(60)
+
+        self.gravity_input.editingFinished.connect(self.change_gravity)
+        self.gravity_slider.valueChanged.connect(self.update_gravity)
+
+        layout = QVBoxLayout()
+
+        layout.addWidget(air_density_label)
+
+        air_density_row = QHBoxLayout()
+        air_density_row.addWidget(self.air_density_slider)
+        air_density_row.addWidget(self.air_density_input)
+
+        layout.addLayout(air_density_row)
+
+        layout.addWidget(gravity_label)
+
+        gravity_row = QHBoxLayout()
+        gravity_row.addWidget(self.gravity_slider)
+        gravity_row.addWidget(self.gravity_input)
+
+        layout.addLayout(gravity_row)
+
+        self.setLayout(layout)
+
+    def update_air_density(self, value):
+        air_density = value / 1000
+        self.air_density_input.setText(f"{air_density:.3f}")
+        self.main_window.change_air_density_size(air_density)
+
+    def change_air_density(self):
+        try:
+            value = float(self.air_density_input.text())
+            slider_value = int(value * 1000)
+            self.air_density_slider.setValue(slider_value)
+        except ValueError:
+            pass
+
+    def update_gravity(self, value):
+        gravity = value / 1000
+        self.gravity_input.setText(f"{gravity:.3f}")
+        self.main_window.change_gravity_size(gravity)
+
+
+    def change_gravity(self):
+        try:
+            value = float(self.gravity_input.text())
+            slider_value = int(value * 1000)
+            self.gravity_slider.setValue(slider_value)
+        except ValueError:
+            pass
--- main.py
+++ main.py
@@ -1,22 +1,123 @@
+
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 from settings import Settings
+from basicparameters import BasicParameters
 
 
 class Program(QWidget):
     def __init__(self):
         super().__init__()
+
         self.setWindowTitle("Projectile Motion sim")
-        self.background = QPixmap("H:/Untitled design.png")
+        self.background = QPixmap("background.png")
         self.resize(800, 500)
+
         self.settings = Settings()
+        self.settings_window = None
+
+        self.settings_panel = QPushButton("⚙ Settings    ▼")
+        self.settings_panel.clicked.connect(self.toggle_settings)
+
+        self.settings_panel.setFixedSize(150, 40)
+
+        self.settings_menu = QFrame()
+
+        self.settings_menu.setStyleSheet("""
+            QFrame {
+                background-color: #c2c1c2;
+                border: 1px solid #cccccc;
+                border-radius: 8px;
+            }
+
+            QPushButton {
+                background-color: #c2c1c2;
+                color: black;
+                border: grey;
+                padding: 8px;
+                text-align: left;
+                border-radius: 4px;
+            }
+
+            QPushButton:hover {
+                background-color: #eeeeee;
+            }
+        """)
+
+        settings_layout = QVBoxLayout()
+
+        settings_layout.setContentsMargins(8, 8, 8, 8)
+        settings_layout.setSpacing(4)
+
+        self.settings_menu.setLayout(settings_layout)
+
+        self.basic_parameters_button = QPushButton("Basic Parameters")
+        self.visual_display_button = QPushButton("Visual Displays")
+        self.cannon_settings_button = QPushButton("Cannon Settings")
+        self.cannonball_details_button = QPushButton("Cannonball Details")
+
+        settings_layout.addWidget(self.basic_parameters_button)
+        settings_layout.addWidget(self.visual_display_button)
+        settings_layout.addWidget(self.cannon_settings_button)
+        settings_layout.addWidget(self.cannonball_details_button)
+
+        self.basic_parameters_button.clicked.connect(self.unfold_settings_panel)
+
+        self.settings_menu.setVisible(False)
+
+        self.settings_container = QWidget()
+
+        settings_container_layout = QVBoxLayout()
+
+        settings_container_layout.setContentsMargins(0, 15, 20, 0)
+        settings_container_layout.setSpacing(5)
+        settings_container_layout.setAlignment(Qt.AlignTop | Qt.AlignRight)
+
+        settings_container_layout.addWidget(self.settings_panel)
+        settings_container_layout.addWidget(self.settings_menu)
+
+        self.settings_container.setLayout(settings_container_layout)
+
+        layout = QVBoxLayout()
+
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        layout.addWidget(self.settings_container)
+
+        layout.addStretch()
+
+        layout.addStretch()
+
+        self.setLayout(layout)
+
+    def toggle_settings(self):
+        is_visible = self.settings_menu.isVisible()
+
+        self.settings_menu.setVisible(not is_visible)
+
+        if is_visible:
+            self.settings_panel.setText("⚙ Settings    ▶")
+        else:
+            self.settings_panel.setText("⚙ Settings    ▼")
+
+    def unfold_settings_panel(self):
+        self.settings_window = BasicParameters(self)
+        self.settings_window.show()
+
+    def change_air_density_size(self, value):
+        print("Air density:", value)
+
+    def change_gravity_size(self, value):
+        print("Gravity:", value)
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        scaled = self.background.scaled(self.size(), aspectRatioMode=2)
+        scaled = self.background.scaled(self.size(),aspectRatioMode=2)
         painter.drawPixmap(0, 0, scaled)
+
 
 app = QApplication(sys.argv)
 window = Program()
